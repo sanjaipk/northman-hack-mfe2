662fd8904cba0c0eea248e2a7969f9248f5d908e
= <=>
2023-01-29 17:33:40 -0500

    ios app enabled

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'northman-testing-shell2',
+  webDir: 'dist/northman-testing-shell2',
+  bundledWebRuntime: false
+};
+
+export default config;
